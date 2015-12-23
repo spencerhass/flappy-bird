5be4c3b89dd44c2b0b13c808ec477fc8a9e63af4
spencerhass <shass615@yahoo.com>
2015-12-23 12:11:59 -0500

    changes to js files

---
+++ site/js/components/graphics/pipe.js
@@ -0,0 +1,10 @@
+var PipeGraphicsComponent = function(entity) {
+    this.entity = entity;
+};
+
+PipeGraphicsComponent.prototype.draw = function() {
+	
+   
+};
+
+exports.PipeGraphicsComponent = PipeGraphicsComponent;
--- site/js/flappy_bird.js
+++ site/js/flappy_bird.js
@@ -1,13 +1,18 @@
 var graphicsSystem = require('./systems/graphics');
+var physicsSystem = require ('./systems/physics');
 var bird = require('./entities/bird');
+var pipe= require('./entities/pipe');
+
 
 var FlappyBird = function() {
-    this.entities = [new bird.Bird()];
+    this.entities = [ new bird.Bird()];
     this.graphics = new graphicsSystem.GraphicsSystem(this.entities);
+    this.physics= new physicsSystem.PhysicsSystem(this.entities);
 };
 
 FlappyBird.prototype.run = function() {
     this.graphics.run();
+    this.physics.run();
 };
 
 exports.FlappyBird = FlappyBird;
